43d733dbdbdc4f9f0155c05e9b3ac22cbb64ca27
chrystalight <catieeleanor@gmail.com>
2025-08-28 09:14:42 -0400

    Added Kinematic Model file for Ali to edit

--- python_gui/q4_pl.py
+++ python_gui/q4_pl.py
@@ -32,8 +32,8 @@ def get_steps_L(curr_theta, delta_theta, latest_dir):
         print(f"latest_dir didnt change or dir_comp is off: latest_dir = {latest_dir} delta theta = {delta_theta} and dr comp = {cf.DIR_COMP}")
         print("oopsies")
     
-    motor_steps[cf.MotorIndex.LJL] = steps
-    motor_steps[cf.MotorIndex.LJR] = -steps
+    motor_steps[cf.MotorIndex.LJL] = -steps
+    motor_steps[cf.MotorIndex.LJR] = steps
     
     return motor_steps, latest_dir
 
@@ -63,6 +63,6 @@ def get_steps_R(curr_theta, delta_theta, latest_dir):
         print(f"latest_dir didnt change or dir_comp is off: latest_dir = {latest_dir} delta theta = {delta_theta} and dr comp = {cf.DIR_COMP}")
         print("oopsies")
     
-    motor_steps[cf.MotorIndex.RJL] = -steps
-    motor_steps[cf.MotorIndex.RJR] = steps
+    motor_steps[cf.MotorIndex.RJL] = steps
+    motor_steps[cf.MotorIndex.RJR] = -steps
     return motor_steps, latest_dir
